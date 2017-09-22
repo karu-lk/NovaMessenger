f8a3d8815010420fb4de4df750f029740892f6b8
Lilupa Karunarathne <lilupa@live.com>
2017-09-22 17:14:18 +1200

    Adding Cognito Fed

--- NovaMessenger.UI/src/app/views/login/login.component.ts
+++ NovaMessenger.UI/src/app/views/login/login.component.ts
@@ -13,38 +13,38 @@ export class LoginComponent implements OnInit {
   }
 
   registerCognitoUser() {
-    AWSCognito.config.region = 'us-east-1'; //This is required to derive the endpoint
+    // AWSCognito.config.region = 'us-east-1'; //This is required to derive the endpoint
 
-    var poolData = {
-      UserPoolId: 'us-east-1_TcoKGbf7n',
-      ClientId: '4pe2usejqcdmhi0a25jp4b5sh3'
-    };
-    var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+    // var poolData = {
+    //   UserPoolId: 'us-east-1_TcoKGbf7n',
+    //   ClientId: '4pe2usejqcdmhi0a25jp4b5sh3'
+    // };
+    // var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
 
-    var attributeList = [];
+    // var attributeList = [];
 
-    var dataEmail = {
-      Name: 'email',
-      Value: 'email@mydomain.com'
-    };
-    var dataPhoneNumber = {
-      Name: 'phone_number',
-      Value: '+15555555555'
-    };
-    var attributeEmail = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserAttribute(dataEmail);
-    var attributePhoneNumber = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserAttribute(dataPhoneNumber);
+    // var dataEmail = {
+    //   Name: 'email',
+    //   Value: 'email@mydomain.com'
+    // };
+    // var dataPhoneNumber = {
+    //   Name: 'phone_number',
+    //   Value: '+15555555555'
+    // };
+    // var attributeEmail = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserAttribute(dataEmail);
+    // var attributePhoneNumber = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserAttribute(dataPhoneNumber);
 
-    attributeList.push(attributeEmail);
-    attributeList.push(attributePhoneNumber);
+    // attributeList.push(attributeEmail);
+    // attributeList.push(attributePhoneNumber);
 
-    userPool.signUp('username', 'password', attributeList, null, function (err, result) {
-      if (err) {
-        alert(err);
-        return;
-      }
-      cognitoUser = result.user;
-      console.log('user name is ' + cognitoUser.getUsername());
-    });
+    // userPool.signUp('username', 'password', attributeList, null, function (err, result) {
+    //   if (err) {
+    //     alert(err);
+    //     return;
+    //   }
+    //   cognitoUser = result.user;
+    //   console.log('user name is ' + cognitoUser.getUsername());
+    // });
   }
 
 }
